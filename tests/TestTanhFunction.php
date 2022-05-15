8a1b2c28c1e2aa5d187e97d3c340c22354088f7c
Alexandr Krylov <alexandr.krylov@gmail.com>
2022-05-15 08:54:51 +0600

    test tanh

--- tests/TestTanhFunction.php
+++ tests/TestTanhFunction.php
@@ -1,28 +1,37 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use ANNPhp\Domain\Entity\ActivationFunctionInterface;
 use PHPUnit\Framework\TestCase;
+use ANNPhp\Domain\Entity\TanhFunction;
 
-final class EmailTest extends TestCase
+final class TestTanhFunction extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress(): void
+    public function providerTanh()
     {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
+        return [
+            [-2, -0.964027580075817],
+            [-1.5, -0.905148253644866],
+            [-1, -0.761594155955765],
+            [-0.5, -0.46211715726001],
+            [0, 0],
+            [0.5, 0.46211715726001],
+            [1, 0.761594155955765],
+            [1.5, 0.905148253644866],
+            [2, 0.964027580075817],
+        ];
     }
 
-    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    /**
+     * @dataProvider providerTanh
+     */
+    public function testTanh($arg, $value): void
     {
-        $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString(): void
-    {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
-        );
+        $activation = new TanhFunction();
+        $this->assertInstanceOf(ActivationFunctionInterface::class, $activation);
+        $this->assertEqualsWithDelta($value, $activation->activation($arg), 0.00001);
     }
 }
