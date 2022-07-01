14e397f86504011e3729467c98ed8d6e6678068b
Alexandr Krylov <alexandr.krylov@gmail.com>
2022-07-01 23:33:32 +0600

    readme update

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ Arrtifical Neuron Network
 ## How to install
 git clone 
 
-composer install do not work
+sudo docker-compose up
+
+sudo docker exec -it ann_app composer install
+
+## linter
+sudo docker exec -it ann_app composer code-sniffer
+
+## testing
+
+sudo docker exec -it ann_app composer test
+
 ## How to use
 
 ### Learning
---
+++ tests/TestOutputService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TestOutputService extends TestCase
+{
+    public function providerGetOutput()
+    {
+    }
+
+    /**
+     * @param $input
+     * @param $output
+     * @return void
+     * @dataProvider providerGetOutput
+     */
+    public function testGetOutput($input, $output)
+    {
+    }
+}
